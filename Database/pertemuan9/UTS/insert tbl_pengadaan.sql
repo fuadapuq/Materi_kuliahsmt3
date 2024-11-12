14455269cc47dc3d6d1607eaea7b5667186f9f5e
fuadapuq <fuazis34@gmail.com>
2024-11-12 11:33:56 +0700

    uts

---
+++ Database/pertemuan9/UTS/insert tbl_pengadaan.sql
@@ -0,0 +1,6 @@
+INSERT INTO tgl_pengadaan 
+(kode_pengadaan,tgl_pengadaan,kode_supplier)
+VALUES
+('PGD003', '2024-10-16','SUP001'),
+('PGD001', '2024-10-15','SUP001'),
+('PGD002', '2024-10-15','SUP002'),
